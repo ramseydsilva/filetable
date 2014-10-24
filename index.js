400654a6476099757e83744f9a2b62974e8fb64b
Ramsey D'silva <rdsilva@venasolutions.com>
2014-10-24 02:28:15 -0400

    Render tree table with one level

--- index.js
+++ index.js
@@ -16,9 +16,102 @@
     }
 }(this, function(root, Filetable, _, $) {
 
+    var that;
+
+    var getRow = function(cols) {
+        var row = $("<tr></tr>");
+        cols.forEach(function(col) {
+            row.append("<td>"+col+"</td>");
+        });
+        return row;
+    };
+
+    var getRowData = function(data, col_keys) {
+        var rows = [];
+        data.forEach(function(rowData) {
+            if (typeof rowData == "object") {
+                var row = [];
+                if (typeof col_keys == "object" && col_keys.length > 0) {
+                    col_keys.forEach(function(key) {
+                        row.push(rowData[key]);
+                    });
+                } else {
+                    row = _.values(rowData);
+                }
+                rows.push(row);
+            } else {
+                throw new Error("data should contain object or array");
+            }
+        });
+        return rows;
+    };
+
+    var getRows = function(data, col_keys) {
+        if (typeof data == "object" && data.length > 0) {
+            return getRowData(data, col_keys);
+        } else if (typeof data == "function") {
+            return getRowData(data(), col_keys);
+        } else if (data && data.length !== 0) {
+            throw new Error("data should be array or function")
+        }
+        return [];
+    };
+
+    var attachRows = function($root, rows, renderCallback) {
+        rows.forEach(function(row) {
+            var $row = getRow(row);
+            $root.append($row);
+            if (typeof renderCallback == "function") {
+                renderCallback.apply(that, $row);
+            }
+        });
+    };
+
     $.prototype.filetable = function(options) {
-        this.append("<table></table>");
+        that = this;
+        this.html("<table><thead></thead><tbody></tbody></table>");
+        var thead = this.find("thead"),
+            tbody = this.find("tbody"),
+            rows;
+
+        if (options) {
+            var rows;
+
+            // Creates header
+            if (typeof options.header == "object") {
+                var header = options.header;
+                    col_keys = _.keys(header);
+
+                if (header && col_keys.length > 0) {
+                    col_keys.forEach(function(key) {
+                        var _class = header[key].class || "";
+                        if (_class) {
+                            _class = "class='"+header[key].class+"'";
+                        }
+                        thead.append("<tr><th "+_class+">"+header[key].html+"</th></tr>");
+                    });
+                    rows = getRows(options.data, col_keys);
+                }
+            } else {
+                // Without header
+                rows = getRows(options.data);
+            }
+
+            if (rows && rows.length > 0) {
+                attachRows(tbody, rows, options.render);
+            }
+
+            // Attach class
+            if (typeof options.class == "string") {
+                this.attr("class", options.class);
+            } else if (!!options.class)  {
+                throw new Error("class should be a string");
+            }
+
+        }
+
         return this;
+
     };
 
     return Filetable;
--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var should = require("should"),
-    $, _;
+    sinon = require("sinon"),
+    $, _, $el;
 
 before(function(done) {
 
@@ -18,14 +19,133 @@ before(function(done) {
 
 describe("$.prototype.filetable", function() {
 
+    before(function() {
+        $el = $("<div></div>");
+        $el.filetable();
+    });
+
     it("exists", function() {
         $.prototype.filetable.should.be.ok;
     });
 
     it("creates table element", function() {
-        var $el = $("<div></div>");
-        $el.filetable({});
         $el.find("table").length.should.be.ok;
+    });
+
+    describe("takes options", function() {
+
+        var options, col_keys;
+
+        before(function() {
+            options = {
+                header: {
+                    "name": {html: "Name"},
+                    "alias": {html: "Alias"},
+                    "operator": {html: "Operator"}
+                },
+                class: "table table-bordered"
+            };
+            $el.filetable(options);
+        });
+
+        it("class", function() {
+            options.class.split(" ").forEach(function(className) {
+                $el.hasClass(className).should.be.true;
+            });
+        });
+
+        describe("header", function() {
+            it("with html", function() {
+                col_keys = _.keys(options.header);
+                col_keys.forEach(function(key) {
+                    $el.find("table thead th:contains('"+options.header[key].html+"')").length.should.be.ok;
+                });
+            });
+
+            it("with class", function() {
+                var opts = _.clone(options);
+                opts.header = {
+                    "name": {html: "Name", class: "name"},
+                    "alias": {html: "Alias", class: "alias"},
+                    "operator": {html: "Operator", class: "operator"}
+                };
+                col_keys = _.keys(opts.header);
+                $el.filetable(opts);
+                col_keys.forEach(function(key) {
+                    $el.find("table thead th."+opts.header[key].class).length.should.be.ok;
+                });
+            });
+        });
+
+        describe("data", function() {
+
+            before(function() {
+                options.data = [
+                    {name: "Ramsey", alias: "Superman", operator: "Kryptonite"}
+                ];
+                $el.filetable(options);
+            });
+
+            var confirmDataOptionObject = function() {
+                $el.find("tbody tr").length.should.be.eql(options.data.length);
+                options.data.forEach(function(data, index) {
+                    var $row = $($el.find("table tbody tr")[index]);
+                    $row.find("td").length.should.be.eql(col_keys.length);
+                    _.keys(data).forEach(function(key) {
+                        $row.find("td:contains('"+data[key]+"')").length.should.be.ok;
+                    });
+                });
+            };
+
+            it("with header option and of type array of objects", function() {
+                confirmDataOptionObject();
+            });
+
+            it("with no header option and of type array of arrays", function() {
+                var opts = _.clone(options);
+                delete opts.header;
+                opts.data = [["Ramsey", "Superman", "Kryptonite"]];
+                $el.filetable(opts);
+                $el.find("tbody tr").length.should.be.eql(opts.data.length);
+                opts.data.forEach(function(data, index) {
+                    var $row = $($el.find("table tbody tr")[index]);
+                    $row.find("td").length.should.be.eql(data.length);
+                    data.forEach(function(col) {
+                        $row.find("td:contains('"+col+"')").length.should.be.ok;
+                    });
+                });
+            });
+
+            it("of type function", function() {
+                var opts = _.clone(options);
+                opts.data = function() {
+                    return options.data;
+                }
+                $el.filetable(opts);
+                confirmDataOptionObject();
+            });
+
+        });
+
+        describe("render", function() {
+
+            var opts, lastRow;
+            before(function() {
+                opts = _.clone(options);
+                opts.data.push({
+                    name: "Bla", alias: "", operator: ""
+                });
+                opts.render = sinon.stub();
+                $el.filetable(opts);
+                opts.render.withArgs($el.find("tbody tr"));
+            });
+
+            it ("called when rendering each row, with row element as first argument", function() {
+                opts.render.callCount.should.be.eql(opts.data.length);
+            });
+
+        });
+
     });
 
 });
